fc7934878488d71d0c5f35def05f5901494af844
Sasha Shuplat <kavabata.y@gmail.com>
2021-01-09 13:22:39 +0100

    rotary controller

--- encoder/main.py
+++ encoder/main.py
@@ -1,64 +1,26 @@
 import config
 import wifi
 import time
+import utime
 import graphqlclient
-import esp
 from machine import Pin, ADC, PWM
-import ure
-import sys
 import math
-import network
 import neopixel
-try:
-  import usocket as socket
-except:
-  import socket
+from urequests import get
 from rotary_irq_esp import RotaryIRQ
 import gc
 gc.collect()
-
-# global controllers properties
-controllers = {
-  'switch': {
-    'url': 'http://192.168.1.187/switch/value/1000',
-    'state': 15,
-    'max': 100,
-    'min': 0,
-    'multiplier': 5
-  }, 
-  'switch2': {
-    'url': 'http://192.168.1.187/switch2/value/1000',
-    'state': 0,
-    'max': 100,
-    'min': 0,
-    'multiplier': 2
-  }
-}
-controller = None
 
 wifi.get_connection()
 addr = wifi.ifconfig()
 config.write_conf('ip_address', addr)
 graphqlclient.send_config_value('ip', addr)
 
-led_cnt = 12
-led_pin = 14
-led = neopixel.NeoPixel(Pin(led_pin), led_cnt)
+stopCycle = True;
 
 # 5, 4, 0 (d1, d2, d3)
-
 rotary_last_state = 0
 rotary_current_state = 0
-
-button_pin = 0
-SHORT_PRESS_TIME = 500
-button = Pin(int(button_pin), Pin.IN)
-button_last_state = 1
-button_current_state = 1
-button_pressed_time  = 0;
-button_released_time = 0;
-
-stopCycle = True;
 
 def set_rotary():
   global rotary
@@ -70,22 +32,9 @@ def set_rotary():
     reverse=False, 
     range_mode=RotaryIRQ.RANGE_WRAP)
 
-
-def loadLed():
-  for x in range(0, led_cnt):
-    led[x] = (2, 5, 1)
-    led.write()
-    time.sleep_ms(25)
-
-  for x in range(0, led_cnt):
-    led[x] = (0, 0, 0)
-    led.write()
-    time.sleep_ms(25)
-
 def get_rotary():
   global rotary_last_state
   global controller
-  global stopCycle
 
   rotary_current_state = rotary.value()
   
@@ -99,17 +48,28 @@ def get_rotary():
     elif(controller['state'] > controller['max']):
       controller['state'] = controller['max']
     
-    print(rotary_last_state, rotary_current_state)
     rotary_last_state = rotary_current_state
-    
+
     return True
 
   return False
-  
-def set_led():
-  global stopCycle
-  global controller
 
+led_cnt = 12
+led_pin = 14
+led = neopixel.NeoPixel(Pin(led_pin), led_cnt)
+
+def load_led():
+  for x in range(0, led_cnt - 1):
+    led[x] = (math.floor(255 / (x+1)), 20 * x, 0)
+    led.write()
+    time.sleep_ms(20)
+
+  for x in range(0, led_cnt):
+    led[x] = (0, 0, 0)
+    led.write()
+    time.sleep_ms(30)
+
+def set_led():
   controller_range = controller['max'] - controller['min']
   led_weight = controller_range / led_cnt
   full_led = math.floor(controller['state'] / led_weight)
@@ -125,21 +85,95 @@ def set_led():
   
   led.write()
 
-def set_url():
+button_pin = 0
+SHORT_PRESS_TIME = 500
+button = Pin(int(button_pin), Pin.IN)
+button_last_state = 1
+button_current_state = 1
+button_pressed_time  = 0
+button_released_time = 0
 
-      
+def get_button():
+  global button_last_state
+  global button_current_state
+  global button_pressed_time
+  global button_released_time
 
-def init():
+  button_current_state = button.value()
+
+  if button_current_state != button_last_state:
+    if button_last_state == 1: # start press
+      button_pressed_time = utime.ticks_ms()
+    else: # release
+      button_released_time = utime.ticks_ms()
+
+    button_last_state = button_current_state
+
+  if button_pressed_time != 0 and button_released_time != 0:
+      duration = button_released_time - button_pressed_time
+      button_pressed_time  = 0
+      button_released_time = 0
+      return duration
+
+  return 0
+
+# global controllers properties
+controllers = {
+  'switch': {
+    'url': 'http://192.168.1.187/switch/value/200',
+    'state': 0,
+    'max': 100,
+    'min': 0,
+    'multiplier': 5
+  }, 
+  'switch2': {
+    'url': 'http://192.168.1.187/switch2/value/200',
+    'state': 0,
+    'max': 100,
+    'min': 0,
+    'multiplier': 2
+  }
+}
+
+controller = None
+controllers_current_key = len(controllers)
+
+def set_next_controller():
   global controller
-  loadLed()
+  global controllers_current_key
+
+  controllers_keys = list(controllers.keys())
+  controllers_current_key += 1
+  if controllers_current_key >= len(controllers_keys):
+    controllers_current_key = 0
+
+  controller = controllers[controllers_keys[controllers_current_key]]
 
   set_rotary()
-  controller = controllers['switch']
+  set_led()
+
+def set_controller():
+  try:
+    get(controller['url'].replace('value', str(controller['state']), 1))
+  except OSError as e:
+    pass
+
+def init():
+  global stopCycle
+  load_led()
+  set_next_controller()
 
   while stopCycle:
+    click = get_button()
+
+    if click > SHORT_PRESS_TIME: #stop
+      stopCycle = False 
+    elif click > 0: #next controller
+      set_next_controller()
+
     if get_rotary():
       set_led()
-      set_url()
+      set_controller()
 
 
 init()
